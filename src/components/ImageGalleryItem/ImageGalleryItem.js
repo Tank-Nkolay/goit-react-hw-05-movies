486d646cbd884197a98e13a7395026b76a323cc6
Nikolay Tankov <tankov.n.k@gmail.com>
2022-10-28 17:52:37 +0300

    modal

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,37 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '../Modal';
 
 import { Li, Img } from './ImageGalleryItem.styled';
 
-class ImageGlleryItem extends React.Component {
-  state = { showModal: false };
+export default function ImageGlleryItem({ item }) {
+  const [showModal, setShowModal] = useState(false);
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
+  const toggleModal = () => {
+    setShowModal(showModal => !showModal);
   };
 
-  render() {
-    const { showModal } = this.state;
-    const { item } = this.props;
-    return (
-      <>
-        <Li id={item.id}>
-          <Img
-            src={item.webformatURL}
-            alt={item.tags}
-            onClick={this.toggleModal}
-          />
-          {showModal && <Modal image={item} toggleModal={this.toggleModal} />}
-        </Li>
-      </>
-    );
-  }
+  return (
+    <>
+      <Li id={item.id}>
+        <Img src={item.webformatURL} alt={item.tags} onClick={toggleModal} />
+        {showModal && <Modal image={item} toggleModal={toggleModal} />}
+      </Li>
+    </>
+  );
 }
-
-export default ImageGlleryItem;
 
 ImageGlleryItem.propTypes = {
   item: PropTypes.shape({
